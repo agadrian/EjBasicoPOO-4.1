db377caef6917d714d6fa4a834517ebfef287597
aarrgom935 <aarrgom935@g.educaand.es>
2024-01-28 14:56:43 +0100

    Codigo creado previo

--- EjBasicoPOO-4.1/src/main/kotlin/Main.kt
+++ EjBasicoPOO-4.1/src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!!")
+    val persona1 = Persona(55.6, 1.80)
+    val persona2 = Persona(70.0, 1.67, "Amancio")
+    val persona3 = Persona(98.4, 1.89, "Juan")
+
+    print("Introduce nuevo nombre de la persona1: ")
+    persona1.nombre = readln()
+
+    println("Nombre: ${persona2.nombre}\nPeso: ${persona2.peso}\nAltura: ${persona2.altura}\nIMC: ${"%.2f".format(persona2.imc)}")
 }
